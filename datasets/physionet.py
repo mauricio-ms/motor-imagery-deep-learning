f82693347ecb5033c1d9478de0186f814bc42710
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2020-08-17 08:49:46 -0300

    Performance enhancement

--- datasets/physionet.py
+++ datasets/physionet.py
@@ -5,11 +5,6 @@ import tensorflow as tf
 import numpy as np
 
 LOGGER = get_logger("physionet.py")
-N_CHANNELS = 64
-FEATURE_DESCRIPTION = {
-    "X": tf.io.FixedLenFeature([N_CHANNELS], tf.float32),
-    "y": tf.io.FixedLenFeature([], tf.int64)
-}
 PHYSIONET_DIR = os.path.join(ROOT_DIR, "data/physionet")
 TFRECORD_FILES_DIR = os.path.join(PHYSIONET_DIR, "preprocessed-tfrecord-files")
 
@@ -25,11 +20,16 @@ def load_data(train_size=0.75, validation_size=None, n_subjects=None, **kwargs):
         train_subjects, validation_subjects = _train_test_split_subjects(train_subjects, 1-validation_size)
         LOGGER.info(f"(Train, Validation, Test) Subjects = "
                     f"({len(train_subjects)}, {len(validation_subjects)}, {len(test_subjects)})")
+        LOGGER.info(f"Train subjects: {train_subjects}")
+        LOGGER.info(f"Validation subjects: {validation_subjects}")
+        LOGGER.info(f"Test subjects: {test_subjects}")
         return _load_set(train_subjects, **kwargs), \
             _load_set(validation_subjects, **kwargs), \
             _load_set(test_subjects, **kwargs)
 
     LOGGER.info(f"(Train, Test) Subjects = ({len(train_subjects)}, {len(test_subjects)})")
+    LOGGER.info(f"Train subjects: {train_subjects}")
+    LOGGER.info(f"Test subjects: {test_subjects}")
     return _load_set(train_subjects, **kwargs), _load_set(test_subjects, **kwargs)
 
 
@@ -39,24 +39,33 @@ def _train_test_split_subjects(subjects, train_size):
 
 
 # TODO - convert_to_2d should define the data directory
-def _load_set(subjects, n_readers=5, n_parse_threads=5, batch_size=100,
-              convert_to_2d=False, expand_dim=False):
+def _load_set(subjects, n_readers=tf.data.experimental.AUTOTUNE,
+              n_parse_threads=tf.data.experimental.AUTOTUNE,
+              batch_size=100, convert_to_2d=False, expand_dim=False):
     path_files = [os.path.join(TFRECORD_FILES_DIR, subject, file_name)
                   for subject in subjects
                   for file_name in sorted(os.listdir(os.path.join(TFRECORD_FILES_DIR, subject)))]
     dataset = tf.data.Dataset.list_files(path_files)
     dataset = dataset.interleave(
-        lambda filepath: tf.data.TFRecordDataset(path_files, compression_type="GZIP"),
-        cycle_length=n_readers)
+        lambda filepath: tf.data.TFRecordDataset(filepath, compression_type="GZIP"),
+        cycle_length=n_readers, num_parallel_calls=n_parse_threads)
+    dataset = dataset.batch(batch_size)
     dataset = dataset.map(lambda r: _preprocess(r, expand_dim=expand_dim),
                           num_parallel_calls=n_parse_threads)
-    return dataset.batch(batch_size).prefetch(1)
+    dataset = dataset.cache()
+    return dataset.prefetch(1)
 
 
-def _preprocess(serialized_eeg_record, expand_dim=False):
-    parsed_eeg_record = tf.io.parse_single_example(serialized_eeg_record, FEATURE_DESCRIPTION)
-    X = parsed_eeg_record["X"]
-    y = parsed_eeg_record["y"]
+@tf.function
+def _preprocess(serialized_eeg_records, expand_dim=False):
+    n_channels = 64
+    feature_description = {
+        "X": tf.io.FixedLenFeature([n_channels], tf.float32),
+        "y": tf.io.FixedLenFeature([], tf.int64)
+    }
+    parsed_eeg_records = tf.io.parse_example(serialized_eeg_records, feature_description)
+    X = parsed_eeg_records["X"]
+    y = parsed_eeg_records["y"]
     if expand_dim:
         X = X[..., np.newaxis]
     return X, y
