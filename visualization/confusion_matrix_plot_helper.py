82f7f8c9fec1090edcae3acbc46b1d5dfcbbea1d
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2020-11-14 10:38:19 -0300

    Colormap removed from confusion matrix plots.

--- visualization/confusion_matrix_plot_helper.py
+++ visualization/confusion_matrix_plot_helper.py
@@ -7,7 +7,7 @@ def plot_confusion_matrix(cm, classes, figure_filepath):
     df_cm = pd.DataFrame(cm, index=classes, columns=classes)
     plt.figure(figsize=(14, 7))
     sn.set(font_scale=1.4)
-    sn.heatmap(df_cm, annot=True, fmt="g", cmap="Blues", annot_kws={"size": 16})
+    sn.heatmap(df_cm, annot=True, fmt="g", cmap="Blues", annot_kws={"size": 16}, cbar=False)
 
     plt.tight_layout()
     plt.savefig(figure_filepath)
