cbe75e7f43d0a6dad6830c7818a2e4a837a984ba
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2020-11-19 21:24:51 -0300

    Files not necessary removed.

--- preprocessing/physionet/config.py
+++ preprocessing/physionet/config.py
@@ -3,5 +3,5 @@ from main import ROOT_DIR
 
 PHYSIONET_DIR = os.path.join(ROOT_DIR, "data/physionet")
 
-DAMAGED_SUBJECTS = ["S089"]
+DAMAGED_SUBJECTS = ["S088", "S089", "S092", "S100", "S104"]
 EXECUTIONS_OF_INTEREST = ["R02", "R04", "R06", "R08", "R10", "R12", "R14"]
